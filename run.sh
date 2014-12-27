b9554edaddff90eb2374a7ae6fea1d939a9d2714
Suchipi Izumi <me@suchipi.com>
2014-12-27 08:40:33 +0000

    update run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --privileged=true -d -p 27015:27015/udp -v /root/gmod-server:/gmod-volume --name gmod-test -e "MAP=gm_flatgrass" -e "ARGS=+rcon_password test" suchipi/gmod-server
+docker run --privileged=true -d -P -v /root/gmod-server:/gmod-volume --name gmod-test -e "MAP=gm_flatgrass" -e "ARGS=+rcon_password test" suchipi/gmod-server
