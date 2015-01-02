b455e44cc07218cda59ead49a2482992e06de19a
Suchipi Izumi <me@suchipi.com>
2015-01-02 21:35:29 +0000

    make build process more agnostic

--- workshop_builder/workshop_builder.sh
+++ workshop_builder/workshop_builder.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-collection_id=$1
-authkey=$2
-gamemode=$3
-map=$4
+base_image=$1
+collection_id=$2
+authkey=$3
+gamemode=$4
+map=$5
 
-outputname=$5
+hostname=$6
+outputname=$7
 
-workshop_requires=${@:6}
+workshop_requires=${@:8}
+
+echo "---"
+echo "Building new workshop image."
+echo "base image: $base_image"
+echo "collection id: $collection_id"
+echo "steam web api key: $authkey"
+echo "gamemode: $gamemode"
+echo "map: $map"
+echo "hostname: $hostname"
+echo "outputname: $outputname"
+echo "workshop requires: $workshop_requires"
+echo "---"
 
 # assemble Dockerfile
-sed "s/collection_id/$1/g" Dockerfile.in > Dockerfile.in2
-sed "s/steam_web_key/$2/g" Dockerfile.in2 > Dockerfile.in3
-sed "s/gamemode/$3/g" Dockerfile.in3 > Dockerfile.in4
-sed "s/map/$4/g" Dockerfile.in4 > Dockerfile
-rm Dockerfile.in2 Dockerfile.in3 Dockerfile.in4
+sed "s/collection_id/$collection_id/g" Dockerfile.in > Dockerfile.in2
+sed "s/steam_web_key/$authkey/g" Dockerfile.in2 > Dockerfile.in3
+sed "s/gamemode/$gamemode/g" Dockerfile.in3 > Dockerfile.in4
+sed "s/map/$map/g" Dockerfile.in4 > Dockerfile.in5
+sed "s/hostname/$hostname/g" Dockerfile.in5 > Dockerfile
+rm Dockerfile.in2 Dockerfile.in3 Dockerfile.in4 Dockerfile.in5
 
 # assemble workshop.lua
 echo "if SERVER then" > workshop.lua
@@ -23,11 +38,11 @@ done
 echo "end" >> workshop.lua
 
 # you have to Ctrl+C manually when the server segfaults at this part. I wish I knew why.
-docker run -it --name workshopbuild suchipi/gmod-server /gmod-base/srcds_run -game garrysmod +maxplayers 1 +host_workshop_collection $collection_id -authkey $authkey +map gm_construct +exit
-docker commit workshopbuild suchipi/gmod-server-workshopbuild
+docker run -it --name workshopbuild $base_image /gmod-base/srcds_run -game garrysmod +maxplayers 1 +host_workshop_collection $collection_id -authkey $authkey +map gm_construct +exit
+docker commit workshopbuild workshopbuild-interim
 docker rm workshopbuild
 docker build -t $outputname .
-docker rmi suchipi/gmod-server-workshopbuild
+docker rmi workshopbuild-interim
 
 rm Dockerfile
 rm workshop.lua
