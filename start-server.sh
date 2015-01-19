727200fa58f074691524d506865385c3b84343af
Suchipi Izumi <me@suchipi.com>
2015-01-19 22:27:22 +0000

    add variables to start script

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 unionfs-fuse -o cow /gmod-volume=RW:/gmod-base=RO /gmod-union
 while true; do
-/gmod-union/srcds_run -game garrysmod +maxplayers ${MAXPLAYERS:=8} "${ARGS}" +map ${MAP:=gm_construct}
+/gmod-union/srcds_run -game garrysmod +maxplayers ${MAXPLAYERS:=16} +hostname ${G_HOSTNAME:="Miniserver"} +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
 done
 
