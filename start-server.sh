f139d334f973a39ec88be69d37d1f22af2f28929
Suchipi Izumi <me@suchipi.com>
2015-01-19 22:43:39 +0000

    add non-privileged functionality to start-server

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-unionfs-fuse -o cow /gmod-volume=RW:/gmod-base=RO /gmod-union
-while true; do
-/gmod-union/srcds_run -game garrysmod +maxplayers ${MAXPLAYERS:=16} +hostname ${G_HOSTNAME:="Miniserver"} +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
-done
+if [[ -z "$NO_UNION" ]]; then
+  unionfs-fuse -o cow /gmod-volume=RW:/gmod-base=RO /gmod-union
+  while true; do
+    /gmod-union/srcds_run -game garrysmod +maxplayers ${MAXPLAYERS:=16} +hostname \"${G_HOSTNAME:="Miniserver"}\" +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
+  done
+else
+  while true; do
+    /gmod-base/srcds_run -game garrysmod +maxplayers ${MAXPLAYERS:=16} +hostname \"${G_HOSTNAME:="Miniserver"}\" +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_$
+  done
+fi
 
