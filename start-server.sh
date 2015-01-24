14984994ef3b370b52de927712d3cbf37ab24b17
Suchipi Izumi <me@suchipi.com>
2015-01-24 03:59:13 +0000

    add norestart and nomaster flags to startup options

--- start-server.sh
+++ start-server.sh
@@ -2,13 +2,13 @@
 if [ -z "$NO_UNION" ]; then
   unionfs-fuse -o cow /gmod-volume=RW:/gmod-base=RO /gmod-union
   while true; do
-    echo running /gmod-union/srcds_run -game garrysmod +maxplayers ${MAXPLAYERS:=16} +hostname \"${G_HOSTNAME:="Miniserver"}\" +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
-    /gmod-union/srcds_run -game garrysmod +maxplayers ${MAXPLAYERS:=16} +hostname \"${G_HOSTNAME:="Miniserver"}\" +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
+    echo running /gmod-union/srcds_run -game garrysmod -norestart -nomaster +maxplayers ${MAXPLAYERS:=16} +hostname \"${G_HOSTNAME:="Miniserver"}\" +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
+    /gmod-union/srcds_run -game garrysmod -norestart -nomaster +maxplayers ${MAXPLAYERS:=16} +hostname \"${G_HOSTNAME:="Miniserver"}\" +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
   done
 else
   while true; do
-    echo running /gmod-base/srcds_run -game garrysmod +maxplayers ${MAXPLAYERS:=16} +hostname \"${G_HOSTNAME:="Miniserver"}\" +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
-    /gmod-base/srcds_run -game garrysmod +maxplayers ${MAXPLAYERS:=16} +hostname \"${G_HOSTNAME:="Miniserver"}\" +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
+    echo running /gmod-base/srcds_run -game garrysmod -norestart -nomaster +maxplayers ${MAXPLAYERS:=16} +hostname \"${G_HOSTNAME:="Miniserver"}\" +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
+    /gmod-base/srcds_run -game garrysmod -norestart -nomaster +maxplayers ${MAXPLAYERS:=16} +hostname \"${G_HOSTNAME:="Miniserver"}\" +gamemode ${GAMEMODE:=sandbox} "${ARGS}" +map ${MAP:=gm_construct}
   done
 fi
 
