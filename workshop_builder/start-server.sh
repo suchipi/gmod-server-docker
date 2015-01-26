b36bc2b41668a14e79089820c74bb0d75c2bfbdf
Suchipi Izumi <me@suchipi.com>
2015-01-26 23:26:33 +0000

    remove workshop builder broken, annoying, inconsistent build output

--- workshop_builder/start-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo $HOSTNAME > /gmod-base/garrysmod/data/container_id.txt
-unionfs-fuse -o cow /gmod-volume=RW:/gmod-base=RO /gmod-union
-while true; do
-/gmod-union/srcds_run -game garrysmod +host_workshop_collection ${COLLECTION_ID} -authkey ${STEAM_WEB_KEY} -norestart -nomaster +maxplayers ${MAXPLAYERS:=16} +hostname ${G_HOSTNAME} "${ARGS}" +gamemode ${GAMEMODE} +map ${MAP}
-done
