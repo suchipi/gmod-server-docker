56fb3395d7a7941b6d3ef0acceb97d4474fbffb7
Suchipi Izumi <me@suchipi.com>
2015-01-31 11:05:29 +0000

    fix bash parsing

--- start-server.sh
+++ start-server.sh
@@ -2,11 +2,11 @@
 if [ -n "$UNION" ]; then
   unionfs-fuse -o cow /gmod-volume=RW:/gmod-base=RO /gmod-union
   while true; do
-    /gmod-union/srcds_run -game garrysmod -norestart -port ${PORT} +maxplayers ${MAXPLAYERS} +hostname \"${G_HOSTNAME}\" +gamemode ${GAMEMODE} "${ARGS}" +map ${MAP}
+    /gmod-union/srcds_run -game garrysmod -norestart -port ${PORT} +maxplayers ${MAXPLAYERS} +hostname "${G_HOSTNAME}" +gamemode ${GAMEMODE} "${ARGS}" +map ${MAP}
   done
 else
   while true; do
-    /gmod-base/srcds_run -game garrysmod -norestart -port ${PORT} +maxplayers ${MAXPLAYERS} +hostname \"${G_HOSTNAME}\" +gamemode ${GAMEMODE} "${ARGS}" +map ${MAP}
+    /gmod-base/srcds_run -game garrysmod -norestart -port ${PORT} +maxplayers ${MAXPLAYERS} +hostname "${G_HOSTNAME}" +gamemode ${GAMEMODE} "${ARGS}" +map ${MAP}
   done
 fi
 
